da836cf71f81b381c4b337e84f4f64a5cd046bc9
Kaja Basheer <kajabasheer90@gmail.com>
2026-06-15 01:44:12 +0530

    Remove Playwright tests and update dependencies

--- tests/testWebsite.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-test.only("Website Testing", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
-  await expect(page).toHaveTitle(
-    "Bas | Lead UI Engineer | React Frontend Developer",
-  );
-  await page.locator("nav").getByText("About").click();
-});
